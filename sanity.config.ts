a11e02dce8a294394f6d1085283549baccd7cf9d
kamil-hassan201 <kamilhassan201@gmail.com>
2022-12-24 00:09:46 +0600

    sanity merge

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
-export default defineConfig({
-  name: "default",
-  title: "blog next13 sanity",
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
-  projectId: "gdfil58u",
-  dataset: "production",
+export default defineConfig({
+  basePath: "/studio",
+  name: "Content_Studio",
+  title: "Content Studio",
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
